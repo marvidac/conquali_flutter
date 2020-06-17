e7af0d056585663b4f59dd90a664ef2d3df0ec71
Marcus Dantas <marvidac.carvalho@unimedfortaleza.com.br>
2020-06-17 08:12:53 -0300

    Script para construir base.

--- lib/sql/db_helper.dart
+++ lib/sql/db_helper.dart
@@ -30,8 +30,16 @@ class DatabaseHelper {
   }
 
   void _onCreate(Database db, int newVersion) async {
-    await db.execute(
-        'CREATE TABLE funcionario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, status BOOL, created TEXT)');
+    await db.execute('CREATE TABLE funcao(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, status BOOL, created TEXT NOT NULL)');
+    await db.execute('CREATE TABLE funcionario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, funcao INTEGER NOT NULL, status BOOL, FOREIGN KEY(funcao) REFERENCES funcao(id), created TEXT NOT NULL)');
+    await db.execute('CREATE TABLE equipe(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, status BOOL, created TEXT NOT NULL)');
+    await db.execute('CREATE TABLE equipe_funcionario(id INTEGER PRIMARY KEY AUTOINCREMENT, equipe INTEGER NOT NULL, funcionario INTEGER NOT NULL, created TEXT NOT NULL, FOREIGN KEY(equipe) REFERENCES equipe(id), FOREIGN KEY(funcionario) REFERENCES funcionario(id) )');
+    await db.execute('CREATE TABLE local(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, status BOOL, created TEXT NOT NULL)');
+    await db.execute('CREATE TABLE item(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, status BOOL, created TEXT NOT NULL)');
+    await db.execute('CREATE TABLE servico(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, status BOOL, created TEXT NOT NULL)');
+    await db.execute('CREATE TABLE servico_item(id INTEGER PRIMARY KEY AUTOINCREMENT, servico INTEGER NOT NULL, item INTEGER NOT NULL, created TEXT NOT NULL, FOREIGN KEY(servico) REFERENCES servico(id), FOREIGN KEY(item) REFERENCES item(id) )');
+    await db.execute('CREATE TABLE inspecao(id INTEGER PRIMARY KEY AUTOINCREMENT, local INTEGER NOT NULL, equipe INTEGER NOT NULL, created TEXT NOT NULL)');
+    await db.execute('CREATE TABLE inspecao_servico_item(id INTEGER PRIMARY KEY AUTOINCREMENT, inspecao INTEGER NOT NULL, servico_item INTEGER NOT NULL, conforme BOOL NOT NULL, obs TEXT, created TEXT NOT NULL, FOREIGN KEY(inspecao) REFERENCES inspecao(id), FOREIGN KEY(servico_item) REFERENCES servico_item(id))');
   }
 
   Future<FutureOr<void>> _onUpgrade(Database db, int oldVersion, int newVersion) async {
